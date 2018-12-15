84f71b872ca809dc0e62aa1248043d43278b59e9
Kristoffer Snabb <kristoffer.snabb@reaktor.com>
2018-12-15 22:16:35 +0200

    testing simple counter note

---
+++ js/testing-simple-counter-app.js
@@ -0,0 +1,16 @@
+/* global describe it */
+
+const expect = chai.expect;
+
+describe("Simple Counter", () => {
+
+  it("should have an initial state of 0", () => {
+    expect(document.querySelector(".app p").textContent).to.equal("The number is now 0");
+  });
+
+  it("should increment the number when the increment button is clicked", () => {
+    document.querySelector(".app button").click();
+    expect(document.querySelector(".app p").textContent).to.equal("The number is now 1");
+  });
+
+});
