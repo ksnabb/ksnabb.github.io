c0b5cedb618aee732dd7883fe0bda787076b90f4
Kristoffer Snabb <kristoffer.snabb@reaktor.com>
2018-12-16 14:40:58 +0200

    Seconds elapsed note

---
+++ js/seconds-elapsed-app.js
@@ -0,0 +1,40 @@
+function pubSub() {
+  const listeners = [];
+  return {
+    dispatch: (event) => listeners.forEach((l) => l(event)),
+    subscribe: (fun) => listeners.push(fun)
+  }
+}
+
+function state(initialValue, pubsub) {
+  let val = initialValue;
+
+  function setValue(newValue) {
+    val = newValue;
+    pubsub.dispatch(newValue);
+  }
+
+  function getValue() {
+    return val;
+  }
+
+  return {
+    getValue,
+    setValue
+  }
+}
+
+const appEvents = pubSub();
+const appState = state(0, appEvents);
+
+function timer() {
+  appState.setValue(appState.getValue() + 1);
+  setTimeout(timer, 1000);
+}
+setTimeout(timer, 1000);
+
+function updateTimerView(val) {
+  document.querySelector(".seconds-elapsed").innerText = `seconds elapsed ${val}`;
+}
+
+appEvents.subscribe(updateTimerView);
