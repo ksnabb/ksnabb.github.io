733b52003c8ce2b209ada31fb75d9a7fbbb28e0b
Kristoffer Snabb <kristoffer.snabb@reaktor.com>
2018-12-18 13:32:29 +0200

    The input sync not and the framework note

---
+++ js/framework.js
@@ -0,0 +1,25 @@
+function pubSub() {
+  const listeners = [];
+  return {
+    dispatch: (event) => listeners.forEach((l) => l(event)),
+    subscribe: (fun) => listeners.push(fun)
+  }
+}
+
+function state(initialValue, pubsub) {
+  let val = initialValue;
+
+  function setValue(newValue) {
+    val = newValue;
+    pubsub.dispatch(newValue);
+  }
+
+  function getValue() {
+    return val;
+  }
+
+  return {
+    getValue,
+    setValue
+  }
+}
---
+++ js/input-sync.js
@@ -0,0 +1,13 @@
+const appEvents = pubSub();
+const appState = state("", appEvents);
+
+const input = document.querySelector('.app input');
+const paragraph = document.querySelector('.app p');
+
+input.addEventListener('input', (evt) => {
+  appState.setValue(evt.srcElement.value);
+});
+
+appEvents.subscribe((newValue) => {
+  paragraph.textContent = newValue;
+});
